99739e012cb417a21cb833e3d68054823f9d4daf
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2020-08-13 12:57:34 +0900

    Adding LICENSE headers etc

--- include/rosilo_robot_driver/rosilo_robot_driver.h
+++ include/rosilo_robot_driver/rosilo_robot_driver.h
@@ -2,20 +2,20 @@
 /*
 # Copyright (c) 2016-2020 Murilo Marques Marinho
 #
-#    This file is part of rosilo_denso_communcation.
+#    This file is part of rosilo_robot_driver.
 #
-#    rosilo_denso_communcation is free software: you can redistribute it and/or modify
+#    rosilo_robot_driver is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    rosilo_denso_communcation is distributed in the hope that it will be useful,
+#    rosilo_robot_driver is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Lesser General Public License for more details.
 #
 #    You should have received a copy of the GNU Lesser General Public License
-#    along with rosilo_denso_communcation.  If not, see <https://www.gnu.org/licenses/>.
+#    along with rosilo_robot_driver.  If not, see <https://www.gnu.org/licenses/>.
 #
 # ################################################################
 #
@@ -43,7 +43,7 @@ protected:
     RobotDriver(const RobotDriver&)=delete;
 public:
     virtual VectorXd get_joint_positions() = 0;
-    virtual void set_joint_positions(const VectorXd& desired_joint_positions_rad) = 0;
+    virtual void set_target_joint_positions(const VectorXd& set_target_joint_positions_rad) = 0;
 
     virtual void connect()=0;
     virtual void disconnect()=0;
