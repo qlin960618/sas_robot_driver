c8d9ede6bb0b0666bcc71692e5529cd5f9e278b2
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2023-05-21 17:16:05 +0900

    [sas_robot_driver_ros.cpp] Improved the documentation of the class. (#2)
    * [sas_robot_driver_ros.cpp] Improved the documentation of the class.
    
    * [sas_robot_driver_ros.cpp] Fixed typo.

--- src/sas_robot_driver_ros.cpp
+++ src/sas_robot_driver_ros.cpp
@@ -1,5 +1,5 @@
 /*
-# Copyright (c) 2016-2022 Murilo Marques Marinho
+# Copyright (c) 2016-2023 Murilo Marques Marinho
 #
 #    This file is part of sas_robot_driver.
 #
@@ -20,7 +20,21 @@
 #
 #   Author: Murilo M. Marinho, email: murilomarinho@ieee.org
 #
-# ################################################################*/
+# ################################################################
+#
+#
+#
+# Contributors:
+#      1. Murilo M. Marinho (murilomarinho@ieee.org)
+#         Responsible for the original implementation.
+#
+#      2. Juan Jose Quiroz Omana (juanjqogm@gmail.com) 
+#         Added documentation.
+#      
+# ################################################################
+*/
+
+
 #include <sas_robot_driver/sas_robot_driver_ros.h>
 #include <dqrobotics/utils/DQ_Math.h>
 #include <dqrobotics/interfaces/json11/DQ_JsonReader.h>
@@ -28,6 +42,47 @@
 namespace sas
 {
 
+/**
+ * @brief Constructor of the RobotDriverROS class.
+ * 
+ * @param nodehandle The roscpp's interface for creating subscribers, publishers, etc.
+ * @param robot_driver The robot driver class of your robot, which must be a concrete class of the RobotDriver class.
+ * @param configuration The configuration of the ROS robot driver.
+ * @param kill_this_node Flag used to kill the current node. 
+ * 
+ *      Example of usage:
+ *
+ *      ros::NodeHandle nh;
+ *      static std::atomic_bool kill_this_process(false);
+ *      
+ *      // Create a RobotDriverROSConfiguration struct
+ *      sas::RobotDriverROSConfiguration robot_driver_ros_configuration;
+ *      std::vector<double> q_min = {-2.3093,-1.5133,-2.4937, -2.7478,-2.4800, 0.8521, -2.6895};
+ *      std::vector<double> q_max = { 2.3093, 1.5133, 2.4937, -0.4461, 2.4800, 4.2094,  2.6895};
+ *      robot_driver_ros_configuration.thread_sampling_time_nsec = 4000000;
+ *      robot_driver_ros_configuration.q_min = q_min;
+ *      robot_driver_ros_configuration.q_max = q_max;
+ *      robot_driver_ros_configuration.robot_driver_provider_prefix = ros::this_node::getName();
+ * 
+ *      // Create an object of RobotDriverMyRobot, where RobotDriverMyRobot is the class that manages
+ *      // the communication with your robot. RobotDriverMyRobot must be a concrete class of RobotDriver()
+ *      // A driver example for the Densowave VS050 robot manipulator (RobotDriverDenso) is provided here:
+ *      // https://github.com/SmartArmStack/sas_robot_driver_denso/blob/master/include/sas_robot_driver_denso/sas_robot_driver_denso.h
+ *      
+ *      
+ *      sas::RobotDriverMyRobot robot_driver_my_robot(custom_struct_for_my_robot_configuration, 
+ *                                                       &kill_this_process);
+ *
+ *      
+ *      sas::RobotDriverROS robot_driver_ros(nh,
+ *                                            &robot_driver_my_robot,
+ *                                            robot_driver_ros_configuration,
+ *                                            &kill_this_process);
+ * 
+ *     A complete example is provided in 
+ *     https://github.com/SmartArmStack/sas_robot_driver_denso/blob/master/src/sas_robot_driver_denso_node.cpp
+ * 
+ */
 RobotDriverROS::RobotDriverROS(ros::NodeHandle &nodehandle,
                                RobotDriver* robot_driver,
                                const RobotDriverROSConfiguration &configuration,
@@ -41,6 +96,14 @@ RobotDriverROS::RobotDriverROS(ros::NodeHandle &nodehandle,
 
 }
 
+
+/**
+ * @brief This method starts the communication loop with the robot. 
+ *        At this stage, your robot is ready to be commanded. 
+ *        
+ * 
+ * @return A return 0 means that the program did what it was intended to do.
+ */
 int RobotDriverROS::control_loop()
 {
     try{
@@ -86,6 +149,10 @@ bool RobotDriverROS::_should_shutdown() const
 }
 
 
+/**
+ * @brief Destructor of  RobotDriverROS class. This method stops the communication with the robot
+ * 
+ */
 RobotDriverROS::~RobotDriverROS()
 {
     robot_driver_->deinitialize();
